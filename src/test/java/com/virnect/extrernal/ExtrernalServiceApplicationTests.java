6e38229fe15c05a3b31c3524f4af51a1d2b307a3
Jintae-VIRNECT <93517034+Jintae-VIRNECT@users.noreply.github.com>
2023-08-25 09:45:17 +0900

    initial commit

---
+++ src/test/java/com/virnect/extrernal/ExtrernalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virnect.extrernal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtrernalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
